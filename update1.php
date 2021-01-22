485b66f9232553a8dfcbbb3028e0f2653aa767d1
Aarthi212 <70945036+Aarthi212@users.noreply.github.com>
2021-01-22 19:14:38 +0530

    Add files via upload

---
+++ update1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+crossorigin="anonymous">   
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" 
+crossorigin="anonymous">
+<style>
+    .jumbotron{
+        width:70%;
+        margin: 10% auto;
+        display: flex;
+        justify-content: center;
+        border: 0.08em solid black;
+    }
+    </style>
+</head>
+
+
+<?php 
+
+require_once 'connect.php';
+
+if($_POST) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$msg = $_POST['msg'];
+
+	$sql = "INSERT INTO contact (name, email, phone, msg) VALUES ('$name', '$email', '$phone', '$msg')";
+	if($connect->query($sql) === TRUE) {
+        ?>
+          <div class="jumbotron">
+          <h1 class="display-4">Successfully  Details is Added!</h1>
+          <div class="col-md-4">
+         
+          <a href='index.html'><button class = 'btn btn-warning ' type='button'>Home</button></a>
+                </div>
+        <?php
+        } else {
+            echo "Error " ;
+        }
+        
+        ?>
+    </div>
+    <?php
+        $connect->close();
+    }
+    
+    ?>
+    </html>
